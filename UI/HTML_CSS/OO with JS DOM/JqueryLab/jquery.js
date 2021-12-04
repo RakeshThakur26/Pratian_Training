b8776ab529449fe7ee0e213acf4d61d26e025c1c
RakeshThakur26 <rakesh.thakur.mail99@gmail.com>
2021-12-04 08:41:58 +0530

    Jquery Lab

--- UI/HTML_CSS/OO with JS DOM/JqueryLab/jquery.js
+++ UI/HTML_CSS/OO with JS DOM/JqueryLab/jquery.js
@@ -5,4 +5,40 @@
 //     console.log(val)
 // }
 
+// var data2 = getData();
+// function getData() {
+//     //e.preventDefault();
+//   return  $.getJSON("cars.json", function (data) {
+//         console.log(data);
+//        //data;
+//     });
+// }
+// console.log(data2)
+// for(var i=0; i<data2.length; i++){
+//     console.log(data2[i].carName)
+// }
 
+
+function getData() {
+    var ourReq = new XMLHttpRequest();
+
+    ourReq.open(
+        "GET",
+        "./cars.json"
+    );
+
+    ourReq.onload = function () {
+        if (ourReq.status >= 200 && ourReq.status < 400) {
+          var ourData = JSON.parse(ourReq.responseText);
+            console.log(ourData);
+            render(ourData);
+        }     
+    }
+
+    ourReq.send();
+
+    counter++;
+
+    if(counter > 5)
+        btn.style.display = "none";
+}
